3547ceb9bbe1a9ddfe3180cb44aa1f401b9f96d1
Dan Marius Panaitoaia <DanMarius.Panaitoaia@cognizant.com>
2026-02-18 10:57:44 +0200

    Add CI/CD pipeline with CodeBuild and EKS IRSA integration

---
+++ iac/main/cicd.tf
@@ -0,0 +1,112 @@
+# IAM Role for CodeBuild
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.project_name}-codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# Policies for CodeBuild Role
+resource "aws_iam_role_policy_attachment" "codebuild_ecr" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name = "${var.project_name}-codebuild-policy"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster"
+        ]
+        Resource = aws_eks_cluster.main.arn
+      }
+    ]
+  })
+}
+
+# CodeBuild Project
+resource "aws_codebuild_project" "build" {
+  name          = "${var.project_name}-build"
+  description   = "Build and deploy ${var.project_name}"
+  build_timeout = "15"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true # Required for Docker build
+
+    environment_variable {
+      name  = "ECR_REPOSITORY_URI"
+      value = aws_ecr_repository.claims_service.repository_url
+    }
+
+    environment_variable {
+      name  = "EKS_CLUSTER_NAME"
+      value = aws_eks_cluster.main.name
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "/aws/codebuild/${var.project_name}-build"
+      stream_name = "build-log"
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/danmariusp/nexus-claims-genai-java.git"
+    git_clone_depth = 1
+    buildspec       = "pipelines/buildspec.yml"
+  }
+}
+
+# Grant CodeBuild access to EKS Cluster
+resource "aws_eks_access_entry" "codebuild" {
+  cluster_name      = aws_eks_cluster.main.name
+  principal_arn     = aws_iam_role.codebuild_role.arn
+  kubernetes_groups = []
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "codebuild" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.codebuild_role.arn
+
+  access_scope {
+    type = "cluster"
+  }
+}
